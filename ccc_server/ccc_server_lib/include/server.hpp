9a13168fc9b122f25268b5bee791288d3a8322f4
chetynan@hotmail.com <chetynan@hotmail.com>
2022-06-06 09:18:08 +0000

    Updated

--- ccc_server/ccc_server_lib/include/server.hpp
+++ ccc_server/ccc_server_lib/include/server.hpp
@@ -1,25 +1,42 @@
 #include <boost/program_options.hpp>
-#include <iostream>
-#include <string>
+#pragma once
 
+namespace CCCserver
+{
+
+/**  Language codes to be used with the Greeter class */
+enum class ServerSate
+{
+    listening,
+    crafting,
+    idle,
+    power
+};
+
+/**
+ * @brief A class for saying hello in multiple languages
+ */
 class CCCserver
 {
-public:
+  public:
+    /**
+     * @brief Creates a server object
+     * @param call to start a server object
+     */
+    void start()
+    {
+    }
+    /**
+     * @brief Destorys the server object
+     * @param lang the language to greet in
+     */
+    void stop()
+    {
+    }
+};
 
-void start()
-{
-
-}
-void stop()
-{
-
-}
-
-
+} // namespace CCCserver
 
 private:
-
-
-
-
-};
+}
+;
---
+++ ccc_server/ccc_server_lib/src/server.cpp
@@ -0,0 +1,9 @@
+#include "server.hpp"
+
+using namespace CCCserver;
+
+CCCserver::void()
+{
+    ServerSate CurrentState;
+    CurrentState = idle;
+}
