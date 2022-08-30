dedfa9fc50f8f738271366a370df80b64b66b0ba
Ché <39664001+goonmax@users.noreply.github.com>
2022-08-30 15:39:37 +1000

    Update monitoring.hpp

--- ccc_server/ccc_server_lib/include/monitoring.hpp
+++ ccc_server/ccc_server_lib/include/monitoring.hpp
@@ -1,40 +1,45 @@
-#include "boost/process.hpp"
-#include "states.hpp"
-#include <iostream>
-#pragma once
-namespace Monitor
-{
-/**
- * @brief A class for creating the server engine
- */
-class NetworkEnumeration
-{
-  public:
-    boost::process::ipstream out;
-    boost::process::ipstream err;
-    /*NetworkEnumeration()
-    {
-        std::cout << "NetworkEnumeration object created" << std::endl;
-        ServerState m_state = INITIALIZE;
-    }
-    ~NetworkEnumeration()
-    {
-        std::cout << "NetworkEnumeration  object destoryed" << std::endl;
-    }*/
-    /**
-     * @brief collects the mac addresses of
-       devices on the same network
-     * @param call to collect mac adresses.
-     */
-    void CollectMacaddresses();
-    /**
-     * @brief
-     * @param
-     */
-
-  private:
-    const std::string collect_addresses = "/usr/bin/ip neighbour";
-    std::vector<std::string> collect_addresses_output;
-    std::string line;
-}; // end of class
-} // namespace Monitor
+#include "boost/process.hpp"                                                                                                                                                                                                                                                                                                
+#include "states.hpp"                                                                                                                                                                                                                                                                                                       
+#include <iostream>                                                                                                                                                                                                                                                                                                         
+#pragma once                                                                                                                                                                                                                                                                                                                
+namespace Monitor                                                                                                                                                                                                                                                                                                           
+{                                                                                                                                                                                                                                                                                                                           
+/**                                                                                                                                                                                                                                                                                                                         
+ * @brief A class for creating the server engine                                                                                                                                                                                                                                                                            
+ */                                                                                                                                                                                                                                                                                                                         
+class NetworkEnumeration                                                                                                                                                                                                                                                                                                    
+{                                                                                                                                                                                                                                                                                                                           
+  public:                                                                                                                                                                                                                                                                                                                   
+    boost::process::ipstream out;                                                                                                                                                                                                                                                                                           
+    boost::process::ipstream err;                                                                                                                                                                                                                                                                                           
+    /*NetworkEnumeration()                                                                                                                                                                                                                                                                                                  
+    {                                                                                                                                                                                                                                                                                                                       
+        std::cout << "NetworkEnumeration object created" << std::endl;                                                                                                                                                                                                                                                      
+        ServerState m_state = INITIALIZE;                                                                                                                                                                                                                                                                                   
+    }                                                                                                                                                                                                                                                                                                                       
+    ~NetworkEnumeration()                                                                                                                                                                                                                                                                                                   
+    {                                                                                                                                                                                                                                                                                                                       
+        std::cout << "NetworkEnumeration  object destoryed" << std::endl;                                                                                                                                                                                                                                                   
+    }*/                                                                                                                                                                                                                                                                                                                     
+    /**                                                                                                                                                                                                                                                                                                                     
+     * @brief collects the mac addresses of                                                                                                                                                                                                                                                                                 
+       devices on the same network                                                                                                                                                                                                                                                                                          
+     * @param call to collect mac adresses.                                                                                                                                                                                                                                                                                 
+     */                                                                                                                                                                                                                                                                                                                     
+    void CollectMacaddresses();                                                                                                                                                                                                                                                                                             
+    /**                                                                                                                                                                                                                                                                                                                     
+     * @brief  does a port scan of an address that is given to the function                                                                                                                                                                                                                                                 
+     * @param call to initiate a port scan with nmap or rustscan                                                                                                                                                                                                                                                            
+     */                                                                                                                                                                                                                                                                                                                     
+                                                                                                                                                                                                                                                                                                                            
+    void PortScan();                                                                                                                                                                                                                                                                                                        
+                                                                                                                                                                                                                                                                                                                            
+  private:                                                                                                                                                                                                                                                                                                                  
+    const std::string collect_addresses = "/usr/bin/ip neighbour";                                                                                                                                                                                                                                                          
+    const std::string top1000ports = "/usr/bin/nmap -sCV -Pn -oA  top1000ports";                                                                                                                                                                                                                                            
+    const std::string allports = "/usr/bin/nmap -p- -T4 -oA allport";                                                                                                                                                                                                                                                       
+    std::vector<std::string> collect_addresses_output;                                                                                                                                                                                                                                                                      
+    std::vector<std::string> nmap_output;                                                                                                                                                                                                                                                                                   
+    std::string line;                                                                                                                                                                                                                                                                                                       
+}; // end of class                                                                                                                                                                                                                                                                                                          
+} // namespace Monitor   
