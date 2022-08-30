d7bc88f09ecfdb3caa4b29122a9a0f5bb4d08236
Ché <39664001+goonmax@users.noreply.github.com>
2022-08-30 15:41:00 +1000

    Add files via upload

---
+++ ccc_server/ccc_server_lib/include/listener.hpp
@@ -0,0 +1,34 @@
+#include "states.hpp"
+#include <boost/asio.hpp>
+#include <boost/program_options.hpp>
+#include <iostream>
+#pragma once
+using boost::asio::ip::tcp;
+namespace ReverseShell
+{
+/**
+ * @brief A class for creating the server engine
+ */
+class listener
+{
+  public:
+    /**
+     * @brief Constructor for the ServerEngine object
+     * @param call to create instance of class
+     */
+
+    listener()
+    {
+        std::cout << "listener object created" << std::endl;
+        ServerState m_state = INITIALIZE;
+    }
+    ~listener()
+    {
+        std::cout << "listener object destoryed" << std::endl;
+    }
+
+  private:
+    const std::string rlwrap = "/usr/bin/rlwrap";
+    const std::string socat = "/usr/bin/socat";
+}; // end of class
+} // namespace ReverseShell
