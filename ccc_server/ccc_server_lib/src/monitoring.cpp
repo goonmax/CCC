eeb2e090a6d8589f0f21827bfcea8c09ec0e1523
Ché <39664001+goonmax@users.noreply.github.com>
2022-08-30 15:41:47 +1000

    Add files via upload

--- ccc_server/ccc_server_lib/src/monitoring.cpp
+++ ccc_server/ccc_server_lib/src/monitoring.cpp
@@ -19,4 +19,27 @@ void NetworkEnumeration::CollectMacaddresses()
         std::cerr << e.what() << std::endl;
     }
 }
+//--------------------------------------------------------------------------------
+void NetworkEnumeration::PortScan()
+{
+    try
+    {
+        boost::process::system(top1000ports, boost::process::std_out > out,
+                               boost::process::std_err > err,
+                               boost::process::std_in < stdin);
+        boost::process::system(allports, boost::process::std_out > out,
+                               boost::process::std_err > err,
+                               boost::process::std_in < stdin);
+    }
+    while (std::getline(out, line))
+    {
+        nmap_output.push_back(line);
+    }
+}
+catch (std::exception& e)
+{
+    std::cerr << e.what() << std::endl;
+}
+
 } // namespace Monitor
+//--------------------------------------------------------------------------------
